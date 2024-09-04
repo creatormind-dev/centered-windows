ae18c775a0a967559719ea96136874ef793a32a2
Mind <63318104+creatormind-dev@users.noreply.github.com>
2024-09-03 22:57:49 -0600

    Add basic window instantiation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::{Window, WindowAttributes}
+};
+
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>
+}
+
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(event_loop.create_window(WindowAttributes::default()).unwrap());
+    }
+
+    fn window_event(
+            &mut self,
+            event_loop: &ActiveEventLoop,
+            window_id: winit::window::WindowId,
+            event: winit::event::WindowEvent,
+        ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping...");
+                event_loop.exit();
+            },
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            },
+            _ => ()
+        };
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+
+    event_loop.run_app(&mut app).unwrap();
 }
