81b74bee56aa297534ed48ef2957e534c4d05612
Mind <63318104+creatormind-dev@users.noreply.github.com>
2024-09-10 01:43:02 -0600

    Add get_windows, window_enum_proc and get_win32_window_info

--- src/app_window_info.rs
+++ src/app_window_info.rs
@@ -1,9 +1,27 @@
 use winit::dpi::{LogicalPosition, LogicalSize};
 
 #[cfg(target_os = "windows")]
-use windows::Win32::Foundation::HWND;
+use windows::Win32::{
+	Foundation::{BOOL, HWND, LPARAM, RECT, TRUE},
+	UI::WindowsAndMessaging::{
+		EnumWindows,
+		GetWindowRect,
+		GetWindowTextW,
+		GWL_EXSTYLE,
+		GetWindowLongPtrW,
+		IsIconic,
+		IsWindow,
+		IsWindowVisible,
+		IsZoomed,
+		WS_EX_APPWINDOW,
+		WS_EX_TOOLWINDOW,
+	}
+};
 
 
+/**
+Represents information for an active application window in the operating system.
+ */
 pub struct AppWindowInfo {
 	pub title: String,
 
@@ -12,4 +30,89 @@ pub struct AppWindowInfo {
 
 	#[cfg(target_os = "windows")]
 	hwnd: HWND,
+}
+
+
+/**
+Returns a list of the currently active and visible application windows in the operating system.
+ */
+pub fn get_windows() -> Vec<AppWindowInfo> {
+	let mut windows: Vec<AppWindowInfo> = vec![];
+
+	if cfg!(target_os = "windows") {
+		unsafe {
+			let _ = EnumWindows(Some(window_enum_proc), LPARAM(&mut windows as *mut _ as isize));
+		}
+	}
+
+	return windows;
+}
+
+
+#[cfg(target_os = "windows")]
+unsafe extern "system" fn window_enum_proc(hwnd: HWND, lparam: LPARAM) -> BOOL {
+	let window = match get_win32_window_info(hwnd) {
+		Ok(w) => w,
+		Err(_) => return TRUE,
+	};
+
+	let window_list = &mut *(lparam.0 as *mut Vec<AppWindowInfo>);
+
+	window_list.push(window);
+
+	TRUE
+}
+
+#[cfg(target_os = "windows")]
+unsafe fn get_win32_window_info(hwnd: HWND) -> Result<AppWindowInfo, ()> {
+
+	// Check if the window handle is a valid pointer.
+	if hwnd.is_invalid() {
+		return Err(());
+	}
+
+	// The handle should belong to a window.
+	if IsWindow(hwnd).as_bool() == false {
+		return Err(());
+	}
+
+	let extended_windows_style = GetWindowLongPtrW(hwnd, GWL_EXSTYLE) as u32;
+
+	// Check if the window isn't a toolbar or other type of widget.
+	if (extended_windows_style & WS_EX_TOOLWINDOW.0) != 0 && (extended_windows_style & WS_EX_APPWINDOW.0) == 0 {
+		return Err(());
+	}
+
+	// A window should be visible, otherwise it could be an overlay or hidden process.
+	if IsWindowVisible(hwnd).as_bool() == false {
+		return Err(());
+	}
+
+	// Minimized windows should be skipped to avoid causing problems with the system.
+	// (Plus they can't be rendered in the centering overlay)
+	// Same thing for maximized windows.
+	if IsIconic(hwnd).as_bool() == true || IsZoomed(hwnd).as_bool() == true {
+		return Err(());
+	}
+
+	let mut rect = RECT::default();
+
+	if GetWindowRect(hwnd, &mut rect).is_err() {
+		return Err(());
+	}
+
+	let mut buffer = [0u16; 512];
+	let length = GetWindowTextW(hwnd, &mut buffer);
+
+	// Same as before. Most windows without a title are other type of processes.
+	if length == 0 {
+		return Err(());
+	}
+
+	Ok(AppWindowInfo {
+		hwnd,
+		position: LogicalPosition::new(rect.left, rect.top),
+		size: LogicalSize::new((rect.right - rect.left) as u32, (rect.bottom - rect.top) as u32),
+		title: String::from_utf16_lossy(&buffer[..length as usize]),
+	})
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,14 @@ use winit::event_loop::{ControlFlow, EventLoop};
 fn main() {
     env_logger::init();
 
+    for window in app_window_info::get_windows() {
+        let title = window.title;
+        let position = window.position;
+        let size = window.size;
+
+        println!("Detected window \"{}\" positioned at ({}, {}) with size {}x{}", title, position.x, position.y, size.width, size.height);
+    }
+
     let event_loop = EventLoop::new().unwrap();
     
     event_loop.set_control_flow(ControlFlow::Wait);
