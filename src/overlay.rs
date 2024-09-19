5cc4a3effac2975668db0b16a15f56eb3df21ad8
Mind <63318104+creatormind-dev@users.noreply.github.com>
2024-09-19 00:49:29 -0600

    Revert overlay opacity

--- src/overlay.rs
+++ src/overlay.rs
@@ -385,7 +385,7 @@ impl<'a> State<'a> {
                                     r: 0.0,
                                     g: 0.0,
                                     b: 0.0,
-                                    a: 1.0,
+                                    a: 0.4,
                                 }),
                                 store: wgpu::StoreOp::Store,
                             },
